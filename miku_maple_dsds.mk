5a03aa0e6cda19e0a24fd0c7ac71bb496b2c937f
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-05-05 13:23:05 +0800

    Update and rename lineage_maple_dsds.mk to miku_maple_dsds.mk

--- miku_maple_dsds.mk
+++ miku_maple_dsds.mk
@@ -1,25 +1,18 @@
+# Inherit some common Miku UI stuff.
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/sony/maple_dsds/device.mk)
-
-# Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
-### BOOTANIMATION
-# vendor/lineage/config/common_full_phone.mk
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-# vendor/lineage/config/common.mk
-TARGET_BOOTANIMATION_HALF_RES := true
+# Device
+$(call inherit-product, device/sony/maple_dsds/device.mk)
 
-### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Vendor blobs
+$(call inherit-product, vendor/sony/maple_dsds/maple_dsds-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_maple_dsds
+PRODUCT_NAME := miku_maple_dsds
 PRODUCT_DEVICE := maple_dsds
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8142
