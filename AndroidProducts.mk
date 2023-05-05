bd59a33867d2e4dce97a2bdd57acbc71e93e19e8
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-05-05 13:15:21 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_maple_dsds.mk
+    $(LOCAL_DIR)/miku_maple_dsds.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_maple_dsds-user \
-    lineage_maple_dsds-userdebug \
-    lineage_maple_dsds-eng
+    miku_maple_dsds-user \
+    miku_maple_dsds-userdebug \
+    miku_maple_dsds-eng
